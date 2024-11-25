37d5917196f6209efc1476785d9ef8b44d9a0f3a
BryanARMatheus <bryanassis244@gmail.com>
2024-11-25 09:06:01 -0300

    refactor: Altera nome das classes para incluir sua funcionalidade

---
+++ Exercicios1/src/application/GatoController.java
@@ -0,0 +1,127 @@
+package application;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
+
+public class GatoController extends MainController implements Initializable{
+	private String raça;
+	private String nome;
+	private int idade;
+	
+	@FXML
+	private Label TituloInformação;
+	@FXML
+	private ImageView ImagemInformação;
+	@FXML
+	private Label DescriçãoInformação;
+	@FXML
+	private RadioButton Estrôncio, Ytterbium;
+	@FXML
+	private VBox VboxOpcoes, VboxInformação;
+	@FXML
+	private Label menuLabel;
+	
+	Image estroncio = new Image(getClass().getResourceAsStream("/Imagens/Estroncio.jpg"));
+	Image ytterbium = new Image(getClass().getResourceAsStream("/Imagens/Ytterbium.jpg"));
+	
+	public void check() {
+		if(Estrôncio.isSelected()) {
+			raça = "Maine Coon";
+			nome = "Estrôncio";
+			idade = 5;
+
+			TituloInformação.setText("Estrôncio");
+			ImagemInformação.setImage(estroncio);
+			DescriçãoInformação.setText("Raça: " + raça + "\nNome: " + nome + "\nIdade: " + idade);
+
+		} else if (Ytterbium.isSelected()) {
+			raça = "Angorá Turco";
+			nome = "Ytterbium";
+			idade = 11;
+
+			TituloInformação.setText("Ytterbium");
+			ImagemInformação.setImage(ytterbium);
+			DescriçãoInformação.setText("Raça: " + raça + "\nNome: " + nome + "\nIdade: " + idade);
+
+		}
+	}
+	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		if(Estrôncio.isSelected()) {
+			raça = "Maine Coon";
+			nome = "Estrôncio";
+			idade = 5;
+
+			TituloInformação.setText("Estrôncio");
+			ImagemInformação.setImage(estroncio);
+			DescriçãoInformação.setText("Raça: " + raça + "\nNome: " + nome + "\nIdade: " + idade);
+
+		} else if (Ytterbium.isSelected()) {
+			raça = "Angorá Turco";
+			nome = "Ytterbium";
+			idade = 11;
+
+			TituloInformação.setText("Ytterbium");
+			ImagemInformação.setImage(ytterbium);
+			DescriçãoInformação.setText("Raça: " + raça + "\nNome: " + nome + "\nIdade: " + idade);
+
+		}
+	}
+	
+	public void andar(ActionEvent e) {
+		Image gatoAndar = new Image(getClass().getResourceAsStream("/Imagens/gatoAndar.jpg"));
+		TituloInformação.setText("Andar");
+		ImagemInformação.setImage(gatoAndar);
+		DescriçãoInformação.setText(nome + " está andando");
+	}
+	
+	public void comer(ActionEvent e) {
+		Image gatoComer = new Image(getClass().getResourceAsStream("/Imagens/gatoComer.jpg"));
+		TituloInformação.setText("Comer");
+		ImagemInformação.setImage(gatoComer);
+		DescriçãoInformação.setText(nome + " começou a comer");
+	}
+	
+	public void dormir(ActionEvent e) {
+		Image gatoDormir = new Image(getClass().getResourceAsStream("/Imagens/gatoDormir.png"));
+		TituloInformação.setText("Andar");
+		ImagemInformação.setImage(gatoDormir);
+		DescriçãoInformação.setText("shhh " + nome + " está dormindo");
+	}
+
+	public String getRaça() {
+		return raça;
+	}
+
+	public void setRaça(String raça) {
+		this.raça = raça;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+
+}
---
+++ Exercicios1/src/application/HeroiController.java
@@ -0,0 +1,74 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+
+
+public class HeroiController extends MainController{
+	
+	private int ataque = 100;
+	private int defesa;
+	private int vida;
+	
+	@FXML
+	private Label TituloInformação;
+	@FXML
+	private ImageView ImagemInformação;
+	@FXML
+	private Label DescriçãoInformação;
+	@FXML
+	private VBox VboxOpcoes, VboxInformação;
+	@FXML
+	private Text menuLabel;
+	
+	Image heroiAtacar = new Image(getClass().getResourceAsStream("/Imagens/heroiAtacar.jpg"));
+	Image heroiDefender = new Image(getClass().getResourceAsStream("/Imagens/heroiDefender.jpg"));
+	Image heroiDesviar = new Image(getClass().getResourceAsStream("/Imagens/heroiDesviar.jpg"));
+	
+	public void atacar(ActionEvent e) {
+		TituloInformação.setText("Atacar");
+		ImagemInformação.setImage(heroiAtacar);
+		DescriçãoInformação.setText("O heroi ataca o mostro e dá " + ataque + " de dano!");
+	}
+	
+	public void defender(ActionEvent e) {
+		TituloInformação.setText("Defender");
+		ImagemInformação.setImage(heroiDefender);
+		DescriçãoInformação.setText("O heroi se defende do ataque");
+	}
+	
+	public void desviar(ActionEvent e) {
+		TituloInformação.setText("Desviar");
+		ImagemInformação.setImage(heroiDesviar);
+		DescriçãoInformação.setText("O heroi desvia do ataque");
+	}
+
+	public int getAtaque() {
+		return ataque;
+	}
+
+	public void setAtaque(int ataque) {
+		this.ataque = ataque;
+	}
+
+	public int getDefesa() {
+		return defesa;
+	}
+
+	public void setDefesa(int defesa) {
+		this.defesa = defesa;
+	}
+
+	public int getVida() {
+		return vida;
+	}
+
+	public void setVida(int vida) {
+		this.vida = vida;
+	}
+}
