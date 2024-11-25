be33098ba48312ec200f4e47787b28a31aefa6f2
BryanARMatheus <bryanassis244@gmail.com>
2024-11-25 09:03:39 -0300

    feat: Adiciona arquivos sql

---
+++ Exercicios1/resources/dumpE2.sql
@@ -0,0 +1,30 @@
+USE `e2`;
+
+INSERT INTO arte (metodo, tipo, instrumento) VALUES
+('tradicional', 'manga', 'Pincel');
+
+INSERT INTO elemento (tipo, grau, foco) VALUES
+('fogo', 3, 'ofensivo');
+
+INSERT INTO gato (raca, nome, idade) VALUES
+('Maine Coon', 'Estrôncio', 5),
+('Angorá Turco', 'Ytterbium', 11);
+
+INSERT INTO heroi (ataque, defesa, vida) VALUES
+(30, 96, 360);
+
+INSERT INTO jogo (nome, genero, tema) VALUES
+('Rain world', 'Aventura', 'Distópico');
+
+INSERT INTO metal_de_transicao (nome, numero_atomico, densidade) VALUES
+('Ósmio', 76, 22.59);
+
+INSERT INTO monstro (nome, raca, nivel) VALUES
+('Argoradon', 'dragão', 86);
+
+INSERT INTO navio (nome, classe, ano_de_construcao) VALUES
+('Bismark', 'Couraçado', 1931),
+('Deutschland', 'Cruzador pesado', 1939);
+
+INSERT INTO raposa (raca, habitat, cor) VALUES
+('Raposa do ártico', 'Tundras árticas', 'Branca');
