d3a8b52faaf77ee2ea7c0c475292763d6a42b761
BryanARMatheus <bryanassis244@gmail.com>
2024-11-28 22:36:40 -0300

    feat: Adiciona metodo para mostrar objetos presentes no banco de dados

--- Exercicios1/src/application/ObjetosController.java
+++ Exercicios1/src/application/ObjetosController.java
@@ -2,12 +2,14 @@ package application;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.Scene;
+import javafx.geometry.Pos;
 import javafx.scene.control.Accordion;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TitledPane;
 import javafx.scene.input.MouseEvent;
-import application.DatabaseConnection;
+import javafx.scene.layout.VBox;
+
 import java.sql.*;
 
 public class ObjetosController {
@@ -49,30 +51,86 @@ public class ObjetosController {
     }
 
     @FXML
-    void consultarObjetos(MouseEvent event) throws SQLException {
-        try {
-            String nomeObjeto = AccordionObjetos.getExpandedPane().getText();
-            connection = DatabaseConnection.getConnection(true);
-            String query = "SELECT nome FROM " + nomeObjeto;
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery(query);
-
-            while (resultSet.next()) {
-                String recordName = resultSet.getString("nome");
-
-                Button button = new Button(recordName);
-
-                //AccordionObjetos.getExpandedPane().;
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        } finally {
+    public void consultarObjetos(MouseEvent event) throws SQLException {
+        if (AccordionObjetos.getExpandedPane() != null) {
             try {
-                if (resultSet != null) resultSet.close();
-                if (statement != null) statement.close();
-                if (connection != null) connection.close();
+                TitledPane pane = (TitledPane) AccordionObjetos.getExpandedPane();
+                VBox targetVBox = (VBox) pane.getContent();
+                targetVBox.getChildren().clear();
+
+
+                String query = null;
+                String nomeObjeto = AccordionObjetos.getExpandedPane().getText().toLowerCase();
+                connection = DatabaseConnection.getConnection(true);
+
+                switch (nomeObjeto) {
+                    case "arte":
+                        query = "SELECT id FROM " + nomeObjeto;
+                        break;
+
+                    case "elemento":
+                        query = "SELECT tipo,id FROM " + nomeObjeto;
+                        break;
+
+                    case "heroi":
+                        query = "SELECT id FROM " + nomeObjeto;
+                        break;
+
+                    case "raposa":
+                        query = "SELECT raca FROM " + nomeObjeto;
+                        break;
+
+                    default:
+                        query = "SELECT nome FROM " + nomeObjeto;
+                }
+
+                Statement statement = connection.createStatement();
+                ResultSet resultSet = statement.executeQuery(query);
+
+                while (resultSet.next()) {
+
+                    String recordName = null;
+
+                    switch (nomeObjeto) {
+                        case "arte":
+                            recordName = "Arte " + resultSet.getString("id");
+                            break;
+
+                        case "elemento":
+                            recordName = "Magia de " + resultSet.getString("tipo") + "" + resultSet.getString("id");
+                            break;
+
+                        case "heroi":
+                            recordName = "Heroi" + resultSet.getString("id");
+                            break;
+
+                        case "raposa":
+                            recordName = resultSet.getString("raca");
+                            break;
+
+                        default:
+                            recordName = resultSet.getString("nome");
+                    }
+
+
+                    Button button = new Button(recordName);
+                    button.setPrefWidth(600);
+                    button.setStyle(String.format("-fx-font-size: 29;"));
+                    button.setAlignment(Pos.CENTER_LEFT);
+                    targetVBox.setSpacing(10);
+
+                    targetVBox.getChildren().add(button);
+                }
             } catch (SQLException e) {
-                System.out.println("Erro ao fechar recursos: " + e.getMessage());
+                throw new RuntimeException(e);
+            } finally {
+                try {
+                    if (resultSet != null) resultSet.close();
+                    if (statement != null) statement.close();
+                    if (connection != null) connection.close();
+                } catch (SQLException e) {
+                    System.out.println("Erro ao fechar recursos: " + e.getMessage());
+                }
             }
         }
     }
@@ -92,45 +150,5 @@ public class ObjetosController {
 
     }
 
-    //@FXML
-    /*private void fillDatabase() {
-        LoginDAO loginDAO = new LoginDAO();
-        try {
-            loginDAO.executeSQLFromFile("/assets/sql/schema.sql");
-            Utils.setAlert("CONFIRMATION", "Preenchimento do banco", "Tabelas criadas com sucesso!");
-        } catch (Exception e) {
-            Utils.setAlert("ERROR", "Preenchimento do banco", "Erro ao criar as tabelas: " + e.getMessage());
-        }
-        try {
-            loginDAO.executeSQLFromFile("/assets/sql/dump.sql");
-            Utils.setAlert("CONFIRMATION", "Preenchimento do banco", "As tabelas foram populadas com sucesso!");
-        } catch (Exception e) {
-            Utils.setAlert("ERROR", "Preenchimento do banco", "Erro ao popular as tabelas: " + e.getMessage());
-        }
-    }*/
 
-	/*static Navio navio1 = new Navio("Bismarck","Couraçado", 1939);
-	//static Navio navio2 = new Navio("Deutschland", "Cruzador pesado", 1931);
-	
-	static Elemento elemento1 = new Elemento("Fogo", 3, "Ofensivo");
-	
-	static Monstro monstro1 = new Monstro("Argoradon", "Dragão", 86);
-	
-	static Musica musica1 = new Musica("HUG AND KILL.wav", 218, "Kobaryo - ");
-	static Musica musica2 = new Musica("_1NF3S__0N_.wav", 293, "DM DOKURO - ");
-	static Musica musica3 = new Musica("Sledge.wav", 205, "Dance with the Dead - ");
-	
-	static Jogo jogo1 = new Jogo("Rain world", "Aventura", "Distópico");
-	
-	static Gato gato1 = new Gato("Maine Coon", "Estrôncio", 5);
-	static Gato gato2 = new Gato("Angorá Turco", "Ytterbium", 11);
-	
-	static Raposa raposa1 = new Raposa("Raposa do ártico", "Tundras árticas", "Branca");
-	
-	static MetalTransição metal1 = new MetalTransição("ósmio", 76, 22.59F);
-	
-	static Arte arte1 = new Arte("tradicional", "manga", "Pincel");
-	
-	static Heroi heroi1 = new Heroi(30, 96, 360);
-	*/
 }
