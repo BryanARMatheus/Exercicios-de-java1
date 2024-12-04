b7ad16a9d9e6b198b08333d1837be10ae44b1c9b
BryanARMatheus <bryanassis244@gmail.com>
2024-12-03 21:56:16 -0300

    feat: Adiciona função de modificar objetos

--- Exercicios1/src/application/ObjetosController.java
+++ Exercicios1/src/application/ObjetosController.java
@@ -1,19 +1,24 @@
 package application;
 
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
-import javafx.scene.control.Accordion;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TitledPane;
-import javafx.scene.input.MouseEvent;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.sql.*;
 
-public class ObjetosController {
+public class ObjetosController extends MainController{
+
+    protected Stage stage;
+    protected Scene scene;
+    protected Parent root;
 
     Connection connection = null;
     PreparedStatement statement = null;
@@ -56,6 +61,15 @@ public class ObjetosController {
     @FXML
     private Label LabelNome;
 
+    @FXML
+    private TextField TextFieldInformacao1;
+
+    @FXML
+    private TextField TextFieldInformacao2;
+
+    @FXML
+    private TextField TextFieldInformacao3;
+
 
     @FXML
     public void initialize() {
@@ -76,8 +90,14 @@ public class ObjetosController {
         }
 
     @FXML
-    void abrirTelaAdicionar(ActionEvent event) {
-
+    void abrirTelaAdicionar(ActionEvent event) throws IOException {
+        FXMLLoader loader = FXMLLoader.load(getClass().getResource("AdicionarObjetoScreen.fxml"));
+        root = loader.load();
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("Adicionar objeto");
+        stage.show();
     }
 
     @FXML
@@ -159,7 +179,9 @@ public class ObjetosController {
         connection = DatabaseConnection.getConnection(true);
         String nomeObjeto = AccordionObjetos.getExpandedPane().getText().toLowerCase();
 
-        String queryDelete = "DELETE FROM " + nomeObjeto + " WHERE " + nomeColuna1 + " = " + objetoInformacoes1 + " AND " + nomeColuna2 + " = " + objetoInformacoes2 + " AND " + nomeColuna3 + " = " + objetoInformacoes3 + ";";
+        String queryDelete = "DELETE FROM " + nomeObjeto + " WHERE " + nomeColuna1 + " = " + objetoInformacoes1
+                + " AND " + nomeColuna2 + " = " + objetoInformacoes2
+                + " AND " + nomeColuna3 + " = " + objetoInformacoes3 + ";";
         Statement stmDelete = connection.createStatement();
         ResultSet rsDelete = statement.executeQuery(queryDelete);
 
@@ -179,7 +201,95 @@ public class ObjetosController {
     }
 
     @FXML
-    void modificarObjeto(ActionEvent event) {
+    void modoDeEdicao(ActionEvent event) {
+
+        if (TextFieldInformacao1 == null) {
+            TextFieldInformacao1 = new TextField(objetoInformacoes1);
+            TextFieldInformacao2 = new TextField(objetoInformacoes2);
+            TextFieldInformacao3 = new TextField(objetoInformacoes3);
+        }
+
+        TextFieldInformacao1.setPrefWidth(LabelInformacao1.getWidth());
+        TextFieldInformacao2.setPrefWidth(LabelInformacao2.getWidth());
+        TextFieldInformacao3.setPrefWidth(LabelInformacao3.getWidth());
+
+        LabelInformacao1.setVisible(false);
+        LabelInformacao2.setVisible(false);
+        LabelInformacao3.setVisible(false);
+
+        VBox parent = (VBox) LabelInformacao1.getParent();
+        parent.getChildren().add(1, TextFieldInformacao1);
+        parent.getChildren().add(2, TextFieldInformacao2);
+        parent.getChildren().add(3, TextFieldInformacao3);
+
+        ButtonModificar.setOnAction(eve -> salvarAlteracao(eve));
+        ButtonModificar.setText("Salvar");
+
+    }
+
+    void salvarAlteracao(ActionEvent event){
+
+        ButtonModificar.disableProperty().bind(TextFieldInformacao1.textProperty().isEmpty()
+                .or(TextFieldInformacao2.textProperty().isEmpty())
+                .or(TextFieldInformacao3.textProperty().isEmpty()));
+
+        try {
+            connection = DatabaseConnection.getConnection(true);
+            String nomeObjeto = AccordionObjetos.getExpandedPane().getText().toLowerCase();
+
+            String queryUpdate = "UPDATE " + nomeObjeto + " SET "
+                    + nomeColuna1 + " = ?, "
+                    + nomeColuna2 + " = ?, "
+                    + nomeColuna3 + " = ? "
+                    + "WHERE " + nomeColuna1 + " = ? AND "
+                    + nomeColuna2 + " = ? AND "
+                    + nomeColuna3 + " = ?";
+
+            PreparedStatement stmUpdate = connection.prepareStatement(queryUpdate);
+
+            stmUpdate.setString(1, TextFieldInformacao1.getText());
+            stmUpdate.setString(2, TextFieldInformacao2.getText());
+            stmUpdate.setString(3, TextFieldInformacao3.getText());
+
+            stmUpdate.setString(4, objetoInformacoes1);
+            stmUpdate.setString(5, objetoInformacoes2);
+            stmUpdate.setString(6, objetoInformacoes3);
+
+            stmUpdate.executeUpdate();
+
+            objetoInformacoes1 = TextFieldInformacao1.getText();
+            objetoInformacoes2 = TextFieldInformacao2.getText();
+            objetoInformacoes3 = TextFieldInformacao3.getText();
+
+            consultarObjetos(sqlObjetoNome);
+
+        } catch(SQLException delEx){
+            throw new RuntimeException(delEx);
+        } finally {
+            try {
+                if (resultSet != null) resultSet.close();
+                if (statement != null) statement.close();
+                if (connection != null) connection.close();
+            } catch (SQLException exe) {
+                System.out.println("Erro ao fechar recursos: " + exe.getMessage());
+            }
+        }
+
+        LabelInformacao1.setText(objetoInformacoes1);
+        LabelInformacao2.setText(objetoInformacoes2);
+        LabelInformacao3.setText(objetoInformacoes3);
+
+        VBox parent = (VBox) TextFieldInformacao1.getParent();
+        parent.getChildren().remove(TextFieldInformacao1);
+        parent.getChildren().remove(TextFieldInformacao2);
+        parent.getChildren().remove(TextFieldInformacao3);
+
+        LabelInformacao1.setVisible(true);
+        LabelInformacao2.setVisible(true);
+        LabelInformacao3.setVisible(true);
+
+        ButtonModificar.setOnAction(evento -> modoDeEdicao(event));
+        ButtonModificar.setText("Modificar");
 
     }
 
@@ -259,11 +369,5 @@ public class ObjetosController {
             }
         }
     }
-
-    @FXML
-    void voltarMenu(ActionEvent event) {
-
-    }
-
 
 }
